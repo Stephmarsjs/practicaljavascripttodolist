169f0dee7db947a46c97fc62a57acfffc57e3223
stephmars-js <stephanierodriguez.student@careerdevs.com>
2019-08-28 21:04:46 -0400

    added multiple functions

--- practicaljavascript.js
+++ practicaljavascript.js
@@ -1,2 +1,21 @@
 var todos = ['item1', 'item2', 'item3'];
 
+function displayTodos() {
+
+}
+
+function addTodo(todo) {
+    todos.push(todo)
+  
+}
+
+function changeTodo(position, newValue) {
+    todos[position] = newValue; 
+  
+}
+
+function deleteTodo(position) {
+    todos.splice(position, 1);
+    displayTodos();
+}
+displayTodos();
