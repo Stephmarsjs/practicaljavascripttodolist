30f859294666b4668920edefadee375401012669
stephmars-js <stephanierodriguez.student@careerdevs.com>
2019-08-29 09:53:32 -0400

    added methods with booleans

--- practicaljavascript.js
+++ practicaljavascript.js
@@ -39,3 +39,30 @@ var todoList = {
     }
 };
 todoList.addTodo();
+
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        console.log('My Todos', this.todos);
+    },
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(position, todoText){
+        this.todo[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = todo.Completed;
+        this.displayTodos();
+    }
+}; 
