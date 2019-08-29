9675e3e29b2677692362c8f96f159d9a38cbc8ab
stephmars-js <stephanierodriguez.student@careerdevs.com>
2019-08-28 21:31:49 -0400

    added Objects&methods

--- practicaljavascript.js
+++ practicaljavascript.js
@@ -19,3 +19,23 @@ function deleteTodo(position) {
     displayTodos();
 }
 displayTodos();
+
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function() {
+        console.log('My Todos', this.todos);
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position){
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
+};
+todoList.addTodo();
